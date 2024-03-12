c97b2fe01730630d8920333f449e1e0a7374c44c
Damien Davis-Neff <damiendavisneff@gmail.com>
2024-03-12 18:56:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const  filterArray = (originalArray, differentArray) => {
     return originalArray.filter(element => differentArray.includes(element));
+    // returns new array consisting of objects included in both arrays
 }
 const replaceObjects = (array,replace) => {
-  var newArray = [];
-  for(let a = 0; a < array.length; a++) {
-    var currentItem = replace[array[a]];
-    newArray.push(currentItem);
-  } return newArray;
+    var newArray = [];
+    for(let a = 0; a < array.length; a++) {
+        var currentItem = replace[array[a]];
+        newArray.push(currentItem);
+    } return newArray;
+    // replaces all instances of an object in an array with another
 }
 const removeDuplicates = (array) => {
-  var usedObjects = [];
-  var newArray = [];
-  for(let a = 0; a < array.length; a++) {
-    if(!usedObjects.includes(array[a])) newArray.push(array[a]);
-    usedObjects.push(array[a]);
-  } return newArray;
+    var usedObjects = [];
+    var newArray = [];
+    for(let a = 0; a < array.length; a++) {
+        if(!usedObjects.includes(array[a])) newArray.push(array[a]);
+        usedObjects.push(array[a]);
+    } return newArray;
+    // self explained
+    // removes duplicate objects from an array
+    // ensuring theres only one of each
+    // NOTE: This removes duplicate keys, no matter the value
 }
 
 module.exports = {
